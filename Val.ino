3da7d2636936ef8eb731a4993759063f94a53b7d
Valeria DC <129999795+Valeria2024@users.noreply.github.com>
2023-11-29 14:24:39 -0600

    Create Val.ino

--- Val.ino
+++ Val.ino
@@ -1,31 +1,64 @@
+//# Pr-ctica-2
+//Práctica número 2
+
 #include <LiquidCrystal.h>
 
-int rs = 12;
-int e = 11;
-int d4 = 5;
-int d5 = 4;
-int d6 = 3;
-int d7 = 2;
 
-LiquidCrystal lcd(rs, e, d4, d5, d6, d7);
+const int rs = 12, en = 11, d4 = 5, d5 = 4, d6 = 3, d7 = 2;
+const int inputPin = 7;
 
-void setup() {
-  lcd.begin(16, 2);
+int value = 0;
+
+LiquidCrystal lcd(rs, en, d4, d5, d6, d7);
+
+String matriculas[6] = {"2022", "202222118", "2022", "2022", "2022"," 2022"};
+
+//ìndice de la matricula actual
+int currentIndex=0;
+
+//Estado del botòn en el ùltimo ciclo
+int lastButtonState = LOW;
+
+void setup(){
+  //configura el nùmero de columnas y filas del LCD:
+lcd.begin(16, 2);
+
+  //configura el pin digital 7 como entrada
+pinMode(inputPin, INPUT);
+
+  // imprime la primera matrìcula en el LCD
+lcd.print(" a ");
 }
 
-void loop() {
-  long matriculas[] = {2022, 202222118, 2022, 2022, 2022, 2022};
+void loop()
+{
+  //lee el valor del pin de entrada
+value = digitalRead(inputPin);
 
-  for (int i = 0; i < 6; i++) {
+  //si el botòn acaba de ser presionado 
+if(value)
+{
+    // Si el valor obtenido es alto, se procede a limpiar la pantalla y mostrar un texto el cual es "Limpiando..."
     lcd.clear();
     lcd.setCursor(0, 0);
-    lcd.print("Matricula " + String(i + 1) + ":");
-    
-    lcd.setCursor(0, 1);
-    lcd.print(matriculas[i]);
-    
+    lcd.print("Limpiando...");
     delay(2000);
-  }
+    
+    
+    // Con esto se borra todo en el lcd
+    lcd.clear();
+    
+    // Con esto el valor que tiene actualmente la matricula incrementa
+    currentIndex = (currentIndex + 1) % 6;
+    
+    // Se procede a mostrar la matricula siguiente de algun compañero en el lcd con el fin de continuar con el siguiente paso
+    lcd.print(matriculas[currentIndex]);
+    delay(1000);
+}
+  
+// Asi como esta el boton, se guarda para poder usarse en el proximo ciclo
+  lastButtonState = value;
+}
 
   lcd.clear();
   delay(2000);
